fd70f019d3f76eb2d8893600077fde73441b3ecf
itsxHrisi <122966420+itsxHrisi@users.noreply.github.com>
2025-03-29 16:09:30 +0100

    Primer commit backend

---
+++ src/main/java/proyect/proyectefinal/ProyectefinalApplication.java
@@ -0,0 +1,13 @@
+package proyect.proyectefinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectefinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectefinalApplication.class, args);
+	}
+
+}
